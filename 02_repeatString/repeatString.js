eaf3f62c91bf1a182c198fa06649d88ab1e6cddf
“Manish” <manishpaulsimon@gmail.comgit config --global user.name Your>
2024-04-07 23:42:20 +0200

    Completed Palindrome exercises
    I can definitely see a pattern emerging of solving coding exercises.
    I should get good at this with time and consistency.
    Persistence is key.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,29 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let array = '';
+    for (let i=0;i<num;i++) {
+        array += string;
+    }
+    return array;
 };
+function random() {
+    const number = Math.floor(Math.random() * 1000);
+    return number;
+}
+
+let newArray = repeatString('hey',3);
+let newArray10 = repeatString('hey',10);
+let newArray1 = repeatString('hey',1);
+let newArray0 = repeatString('hey',0);
+let newArrayError = repeatString('hey',-1);
+let newArrayRandom = repeatString('hey',random());
+let newArrayBlank = repeatString('',10);
+
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,36 @@
-const palindromes = function () {
+const palindromes = (word) => {
+    let reverseString = '';
+    let array = [];
+    //Clean up the String first
+    let cleanWord = word.split('').filter(char => 
+        char !== '!' && char !== ',' && char !== ' ' && char !== '.')
+        .join('').toLowerCase();
 
-};
+    // Get length of Cleaned Word    
+    let stringLength = cleanWord.length;
+
+    // running through the cleanedword length and reversing the string
+    for (let i=0; i < stringLength; i++) {
+            // add last character to empty array
+            array.push(cleanWord[stringLength-1-i]);
+    }
+    // convert array to string
+    reverseString = array.join('');
+
+    // Get a clean version of the user's word
+  
+    if (cleanWord == reverseString) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+let String1 = palindromes('racecar');
+let String2 = palindromes('racecar!');
+let String3 = palindromes('A car, a man, a maraca.');
+
 
 // Do not edit below this line
 module.exports = palindromes;
